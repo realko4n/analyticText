dcfd4bad58d002b49e4369d97997269342e47760
realko4n <90069252+realko4n@users.noreply.github.com>
2025-04-07 19:33:25 +0300

    Add loop

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ from spacy.lang.ru import Russian
 from spacy.lang.ru.stop_words import STOP_WORDS
 
 model = Russian()
+
+text = model("Хороший фильм, было интересно смотреть, приятный сюжет")
+
+print(text)
+text_list = [word for word in text]
+print(text_list)
