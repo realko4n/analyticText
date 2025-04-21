4e578eca8664f75a85b735f33579af05e73bb569
realko4n <90069252+realko4n@users.noreply.github.com>
2025-04-21 18:19:16 +0300

    add code

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from spacy.lang.ru import Russian
 from spacy.lang.ru.stop_words import STOP_WORDS
+import ru_core_news_md
+from textblob import TextBlob
+model = ru_core_news_md.load()
 
-model = Russian()
-
-text = model("Хороший фильм, было интересно смотреть, приятный сюжет")
+text = model("Плохо")
 
 print(text)
-text_list = [word for word in text]
+text_list = [word.lemma_ for word in text]
+filter_text_list = [elem for elem in text_list if not elem in STOP_WORDS]
 print(text_list)
+print(filter_text_list)
+
+analys = TextBlob(str(filter_text_list))
+sentiment = analys.sentiment.polarity
+
+if sentiment > 0:
+    print("pos")
+elif sentiment < 0:
+    print("neg")
+else:
+    print("neytral")
---
+++ ww.py
@@ -0,0 +1,5 @@
+snake_body = [133,222,311,34,55, 66]
+
+print(snake_body[2])
+
+print(len(snake_body) - 1)
